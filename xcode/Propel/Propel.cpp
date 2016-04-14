5875599059e96ee9083f6951b929c3fa911632cd
Cyrus Roshan <CyrusRoshan@users.noreply.github.com>
2016-04-14 00:18:43 -0500

    added some basic linked list functions

--- xcode/Propel/Propel.cpp
+++ xcode/Propel/Propel.cpp
@@ -2,7 +2,6 @@
 #include <string.h>
 #include "Propel.hpp"
 
-
 PropelBase::PropelBase() {
 
 }
@@ -14,21 +13,76 @@ PropelBase::PropelNode::PropelNode(std::string name, void* address, int type) {
   this->next = NULL;
 }
 
+std::string PropelBase::PropelNode::toString() {
+    switch (this->type) {
+        case BOOL:
+            return *(bool*)this->address ? "true" : "false";
+            break;
+            
+        case INT:
+            return std::to_string(*(int*)this->address);
+            break;
+            
+        case CHAR:
+            return std::to_string(*(char*)this->address);
+            break;
+            
+        case FLOAT:
+            return std::to_string(*(float*)this->address);
+            break;
+            
+        case DOUBLE:
+            return std::to_string(*(double*)this->address);
+            break;
+            
+        case STRING:
+            return *(std::string*)this->address;
+            break;
+        
+        default:
+            return "Error: invalid or nonexistent type entered";
+    }
+}
+
 PropelBase::PropelDict::PropelDict() {
   this->head = NULL;
+}
+
+PropelBase::PropelNode* PropelBase::PropelDict::getNodeVal(std::string name, int type) {
+    if (this->head) {
+        PropelNode* ptr = this->head;
+        while (ptr->next) {
+            if (ptr->name == name) {
+                return ptr;
+            }
+            ptr = ptr->next;
+        }
+    }
+    return this->head;
+}
+
+PropelBase::PropelNode* PropelBase::PropelDict::setNodeVal(std::string name, void* address, int type) {
+    if (this->head) {
+        PropelNode* ptr = this->head;
+        while (ptr->next) {
+            if (ptr->name == name) {
+                ptr->address = address;
+                ptr->type = type;
+                return ptr;
+            }
+            ptr = ptr->next;
+        }
+    }
+    this->head = new PropelNode(name, address, type);
+    return this->head;
 }
 
 void PropelBase::PropelDict::deleteNode(std::string name) {
     PropelNode* prev = this->head;
     PropelNode* ptr = NULL;
-    if (ptr->name == name) {
-        this->head = this->head->next;
-        delete prev;
-        return;
-    }
     while (prev->next) {
         ptr = prev->next;
-        if (ptr->name == name) { //if string literals are equal
+        if (ptr->name == name) {
             prev->next = ptr->next;
             delete ptr;
             return;
--- xcode/Propel/Propel.hpp
+++ xcode/Propel/Propel.hpp
@@ -7,37 +7,36 @@
 enum {BOOL, INT, CHAR, FLOAT, DOUBLE, STRING};
 
 class PropelBase { //all nested, because let's not interfere with devs' Arduino code more than we have to
-  private:
-    
-    class PropelNode {
-      public:
-        PropelNode();
-        PropelNode(std::string, void*, int);
-        std::string name;
-        void* address;
-        int type;
-        PropelNode* next;
-    };
+    private:
+        class PropelNode {
+            public:
+                PropelNode(std::string, void*, int);
+                std::string toString();
+                std::string name;
+                void* address;
+                int type;
+                PropelNode* next;
+        };
 
-    class PropelDict {
-      public:
-        PropelDict();
-        void* getNode(std::string, int);
-        void setNode(std::string, void*, int);
-        void deleteNode(std::string);
-        PropelNode* head;
-    };
+        class PropelDict {
+            public:
+                PropelDict();
+                PropelNode* getNodeVal(std::string, int);
+                PropelNode* setNodeVal(std::string, void*, int);
+                void deleteNode(std::string);
+                PropelNode* head;
+        };
 
-    PropelDict varDict;
-    void writeMessage(PropelNode*, char*);
-    std::string readMessage();
+        PropelDict varDict;
+        void writeMessage(PropelNode*, char*);
+        std::string readMessage();
     
-  public:
-    PropelBase();
-    void function (std::string, void*);
-    void variable(std::string, void*, int);
-    PropelNode debugFunction(std::string);
-    PropelNode debugVariable(std::string);
+    public:
+        PropelBase();
+        void function (std::string, void*);
+        void variable(std::string, void*, int);
+        PropelNode debugFunction(std::string);
+        PropelNode debugVariable(std::string);
 } extern Propel;
 
 
